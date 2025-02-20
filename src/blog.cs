a2b37ef2f44a289a1071f08275517ecbc7c1d1ae
Cédric Belin <cedric.belin@mc2it.com>
2025-02-20 11:30:03 +0100

    Add the `Blog` class

--- src/blog.cs
+++ src/blog.cs
@@ -1,1 +1,23 @@
 namespace Belin.Akismet;
+
+/// <summary>
+/// Represents the front page or home URL transmitted when making requests.
+/// </summary>
+/// <param name="url">The blog or site URL.</param>
+public class Blog(string url) {
+
+	/// <summary>
+	/// The character encoding for the values included in comments.
+	/// </summary>
+	public string Charset { get; set; } = string.Empty;
+
+	/// <summary>
+	/// The languages in use on the blog or site, in ISO 639-1 format.
+	/// </summary>
+	public IList<string> Languages { get; set; } = [];
+
+	/// <summary>
+	/// The blog or site URL.
+	/// </summary>
+	public Uri Url { get; set; } = new Uri(url);
+}
