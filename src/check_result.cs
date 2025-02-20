d92d3b375ac2464b2e5afccfc9f3331601707e95
Cédric Belin <cedric.belin@mc2it.com>
2025-02-20 11:29:52 +0100

    Add the `CheckResult` enumeration

--- src/check_result.cs
+++ src/check_result.cs
@@ -1,1 +1,22 @@
 namespace Belin.Akismet;
+
+/// <summary>
+/// Specifies the result of a comment check.
+/// </summary>
+public enum CheckResult {
+
+	/// <summary>
+	/// Specifies the result of a comment check.
+	/// </summary>
+	Ham,
+
+	/// <summary>
+	/// The comment is a spam.
+	/// </summary>
+	Spam,
+
+	/// <summary>
+	/// The comment is a pervasive spam (i.e. it can be safely discarded).
+	/// </summary>
+	PervasiveSpam
+}
