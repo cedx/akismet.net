a33e2a302574c850f94466b10613799d6e2f1046
Cédric Belin <cedric.belin@mc2it.com>
2025-03-17 14:33:01 +0100

    Code optimization [skip ci]

--- src/client.cs
+++ src/client.cs
@@ -21,17 +21,17 @@ public class Client(string apiKey, Blog blog, string baseUrl = "https://rest.aki
 	/// <summary>
 	/// The Akismet API key.
 	/// </summary>
-	public string ApiKey { get; } = apiKey;
+	public string ApiKey => apiKey;
 
 	/// <summary>
 	/// The base URL of the remote API endpoint.
 	/// </summary>
-	public Uri BaseUrl { get; } = new Uri(baseUrl.EndsWith('/') ? baseUrl : $"{baseUrl}/");
+	public Uri BaseUrl => new(baseUrl.EndsWith('/') ? baseUrl : $"{baseUrl}/");
 
 	/// <summary>
 	/// The front page or home URL of the instance making requests.
 	/// </summary>
-	public Blog Blog { get; } = blog;
+	public Blog Blog => blog;
 
 	/// <summary>
 	/// Value indicating whether the client operates in test mode.
