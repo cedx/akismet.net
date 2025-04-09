18ebf073207a680578a0bd61bf97ff4ddf4df4d6
Cédric Belin <cedric@belin.io>
2025-04-09 10:22:06 +0200

    Code optimization

--- src/Comment.cs
+++ src/Comment.cs
@@ -59,9 +59,9 @@ public sealed class Comment(Author author) {
 		var map = Author.ToDictionary();
 		if (!string.IsNullOrWhiteSpace(Content)) map["comment_content"] = Content;
 		// TODO if (Context.Count > 0) map["comment_context"] = string.Join(',', Context);
-		if (Date is not null) map["comment_date_gmt"] = Date?.ToUniversalTime().ToString("o")!;
+		if (Date is not null) map["comment_date_gmt"] = Date.Value.ToUniversalTime().ToString("o");
 		if (Permalink is not null) map["permalink"] = Permalink.ToString();
-		if (PostModified is not null) map["comment_post_modified_gmt"] = PostModified?.ToUniversalTime().ToString("o")!;
+		if (PostModified is not null) map["comment_post_modified_gmt"] = PostModified.Value.ToUniversalTime().ToString("o");
 		if (!string.IsNullOrWhiteSpace(RecheckReason)) map["recheck_reason"] = RecheckReason;
 		if (Referrer is not null) map["referrer"] = Referrer.ToString();
 		if (!string.IsNullOrWhiteSpace(Type)) map["comment_type"] = Type;
