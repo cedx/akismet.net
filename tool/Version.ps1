2e92a8c4a84ff2533551b537232fe8d6a7ac738b
Cédric Belin <cedx@outlook.com>
2025-10-09 02:31:50 +0200

    Add a "Lint" script [skip ci]

--- tool/Version.ps1
+++ tool/Version.ps1
@@ -1,4 +1,4 @@
-Write-Host "Updating the version number in the sources..."
+Write-Output "Updating the version number in the sources..."
 $version = (Select-Xml "//Version" Package.xml).Node.InnerText
 (Get-Content "src/Client.cs") -replace 'Version = "\d+(\.\d+){2}"', "Version = ""$version""" | Out-File "src/Client.cs"
 foreach ($item in Get-ChildItem "*/*.csproj") {
