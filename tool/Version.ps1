2f32d6a52f16a5fb7fb6fd528c9212e12f01bdc8
Cédric Belin <cedx@outlook.com>
2025-10-13 11:21:42 +0200

    Update the build system [skip ci]

--- tool/Version.ps1
+++ tool/Version.ps1
@@ -1,5 +1,5 @@
 Write-Output "Updating the version number in the sources..."
-$version = (Import-PowerShellDataFile "Package.psd1").Version
+$version = (Import-PowerShellDataFile "Akismet.psd1").ModuleVersion
 (Get-Content "src/Client.cs") -replace 'Version = "\d+(\.\d+){2}"', "Version = ""$version""" | Out-File "src/Client.cs"
 foreach ($item in Get-ChildItem "*/*.csproj") {
 	(Get-Content $item) -replace "<Version>\d+(\.\d+){2}</Version>", "<Version>$version</Version>" | Out-File $item
