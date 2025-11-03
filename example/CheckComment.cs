04af114c8797dfcb16f4d202fe0a9fecf936b244
Cédric Belin <cedx@outlook.com>
2025-11-03 10:12:15 +0100

    Update the user agent string [skip ci]

--- example/CheckComment.cs
+++ example/CheckComment.cs
@@ -9,7 +9,7 @@ try {
 		Email = "john.doe@domain.com",
 		Name = "John Doe",
 		Role = "guest",
-		UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36 Edg/133.0.0.0"
+		UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:144.0) Gecko/20100101 Firefox/144.0"
 	};
 
 	var comment = new Comment(author) {
