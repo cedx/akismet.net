3bce5aefc5bb05f15a4fe0be152b198b71529da7
Cédric Belin <cedx@outlook.com>
2025-11-22 23:58:20 +0100

    Update the "Lint" script [skip ci]

--- tool/Lint.ps1
+++ tool/Lint.ps1
@@ -1,4 +1,4 @@
 "Performing the static analysis of source code..."
 Import-Module PSScriptAnalyzer
 Invoke-ScriptAnalyzer $PSScriptRoot -Recurse
-Test-ModuleManifest "$PSScriptRoot/../Akismet.psd1" | Out-Null
+Test-ModuleManifest Akismet.psd1 | Out-Null
