7fac805ba46fc6e60897572bdc721135c3a64408
Cédric Belin <cedx@outlook.com>
2025-12-01 21:51:52 +0100

    Update the build system

--- tool/Publish.ps1
+++ tool/Publish.ps1
@@ -1,12 +1,30 @@
-& "$PSScriptRoot/Clean.ps1"
-& "$PSScriptRoot/Version.ps1"
+if ($Release) {
+	& "$PSScriptRoot/Clean.ps1"
+	& "$PSScriptRoot/Version.ps1"
+}
+else {
+	"The ""-Release"" switch must be set!"
+	exit 1
+}
 
 "Publishing the package..."
-$version = (Import-PowerShellDataFile "Akismet.psd1").ModuleVersion
+$module = Import-PowerShellDataFile "Akismet.psd1"
+$version = $module.ModuleVersion
 git tag "v$version"
 git push origin "v$version"
 
-dotnet pack --output var
-foreach ($item in Get-Item "var/*.nupkg") {
+dotnet pack --output var/NuGet
+foreach ($item in Get-Item "var/NuGet/*.nupkg") {
 	dotnet nuget push $item --api-key $Env:NUGET_API_KEY --source https://api.nuget.org/v3/index.json
 }
+
+$name = Split-Path "Akismet.psd1" -LeafBase
+$output = "var/$name"
+New-Item $output/bin -ItemType Directory | Out-Null
+Copy-Item "$name.psd1" $output
+Copy-Item *.md $output
+Copy-Item $module.RootModule $output/bin
+if ("RequiredAssemblies" -in $module.Keys) { Copy-Item $module.RequiredAssemblies $output/bin }
+
+Compress-PSResource $output var/PSGallery
+Publish-PSResource -ApiKey $Env:PSGALLERY_API_KEY -NupkgPath "var/PSGallery/$name.$version.nupkg"
