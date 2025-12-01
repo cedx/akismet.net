58504157db894d22ae35dca0ee541594720553f8
Cédric Belin <cedx@outlook.com>
2025-12-01 22:11:52 +0100

    Update the "Publish" script [skip ci]

--- tool/Publish.ps1
+++ tool/Publish.ps1
@@ -13,18 +13,22 @@ $version = $module.ModuleVersion
 git tag "v$version"
 git push origin "v$version"
 
-dotnet pack --output var/NuGet
-foreach ($item in Get-Item "var/NuGet/*.nupkg") {
+$output = "var/NuGet"
+dotnet pack --output $output
+foreach ($item in Get-Item "$output/*.nupkg") {
 	dotnet nuget push $item --api-key $Env:NUGET_API_KEY --source https://api.nuget.org/v3/index.json
 }
 
-$name = Split-Path "Akismet.psd1" -LeafBase
-$output = "var/$name"
+$output = "var/PSModule"
 New-Item $output/bin -ItemType Directory | Out-Null
-Copy-Item "$name.psd1" $output
+Copy-Item "Akismet.psd1" $output
 Copy-Item *.md $output
 Copy-Item $module.RootModule $output/bin
 if ("RequiredAssemblies" -in $module.Keys) { Copy-Item $module.RequiredAssemblies $output/bin }
 
-Compress-PSResource $output var/PSGallery
-Publish-PSResource -ApiKey $Env:PSGALLERY_API_KEY -NupkgPath "var/PSGallery/$name.$version.nupkg"
+$output = "var/PSGallery"
+New-Item $output -ItemType Directory | Out-Null
+Compress-PSResource var/PSModule $output
+foreach ($item in Get-Item "$output/*.nupkg") {
+	Publish-PSResource -ApiKey $Env:PSGALLERY_API_KEY -NupkgPath $item
+}
