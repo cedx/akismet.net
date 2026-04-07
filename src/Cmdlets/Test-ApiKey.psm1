678855ff03e360073045afb312046aaa088a8f9b
Cédric Belin <cedx@outlook.com>
2026-04-07 23:02:16 +0200

    Port the `Test-ApiKey` cmdlet

--- src/Cmdlets/Test-ApiKey.psm1
+++ src/Cmdlets/Test-ApiKey.psm1
@@ -1,24 +1,25 @@
+using namespace Belin.Akismet
+
 <#
 .SYNOPSIS
 	Checks the API key against the service database, and returns a value indicating whether it is valid.
+.INPUTS
+	The Akismet API key.
+.OUTPUTS
+	`$true` if the specified API key is valid, otherwise `$false`.
 #>
-function Test-ApiKey"), [OutputType([bool))]
-public class TestApiKeyCommand: Cmdlet {
+function Test-ApiKey {
+	[CmdletBinding()]
+	[OutputType([bool])]
+	param (
+		# The Akismet API key.
+		[Parameter(Mandatory, Position = 0, ValueFromPipeline)]
+		[string] $ApiKey,
 
-	<#
-	/// The Akismet API key.
-	#>
-	[Parameter(Mandatory, Position = 0, ValueFromPipeline)]
-	[string ApiKey,
+		# The front page or home URL of the instance making requests.
+		[Parameter(Mandatory)]
+		[Blog] $Blog
+	)
 
-	<#
-	/// The front page or home URL of the instance making requests.
-	#>
-	[Parameter(Mandatory)]
-	[Blog Blog,
-
-	<#
-	/// Performs execution of this command.
-	#>
-	process => WriteObject(new Client(ApiKey, Blog).VerifyKey());
+	[Client]::new($ApiKey, $Blog).VerifyKey()
 }
