4d8f069516f833722cf115e4694b45c26eb2713e
Cédric Belin <cedx@outlook.com>
2026-04-25 12:31:28 +0200

    Remove the PowerShell examples

--- example/Cmdlets/Test-AkismetApiKey.ps1
+++
@@ -1,8 +0,0 @@
-<#
-.SYNOPSIS
-	Verifies an Akismet API key.
-#>
-Import-Module Belin.Akismet
-
-$isValid = Test-AkismetApiKey "123YourAPIKey" -Blog "https://www.yourblog.com"
-Write-Output ($isValid ? "The API key is valid." : "The API key is invalid.")
